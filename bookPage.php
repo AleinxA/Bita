405f316cc964f2b04e074b661d6645874f269a4a
Aleinx <aleinx.official@gmail.com>
2023-05-10 01:51:48 -0500

    creando mas paginas php

---
+++ bookPage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <?php
+    include ('./pagesTemplate/dependecias.php');
+    ?>
+</head>
+<body>
+    <?php
+    include('./pagesTemplate/nav.php');
+    ?>
+    <?php
+    include('./pagesTemplate/footer.php');
+    ?>
+</body>
+</html>
