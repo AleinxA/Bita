f8474d3b3083366da7ebb8a49da0b8fe141c9b72
Aleinx <aleinx.official@gmail.com>
2023-05-12 01:20:49 -0500

    a

--- pagesTemplate/sendMail.php
+++ pagesTemplate/sendMail.php
@@ -1,6 +1,18 @@
 <?php
+$servername = "localhost"; //nombre del servidor
+$username = "Aleinx"; //nombre de usuario de la base de datos
+$password = "Aleinx22@"; //contraseña de la base de datos
+$dbname = "BitaDB"; //nombre de la base de datos
 
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// Comprobar la conexión
+if ($conn->connect_error) {
+  die("La conexión a la base de datos falló: " . $conn->connect_error);
+}
+
+$from = $_POST["emailAddress"];
 $to      = 'aagonzalespolicio@gmail.com';
 $subject = 'Mejoras Bita';
 $message = $_POST["emailText"];
